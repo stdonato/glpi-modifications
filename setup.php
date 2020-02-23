48bf06c79ef51ea4974d13f57923aabd73515910
stdonato <stevenesdonato@gmal.com>
2020-02-23 13:36:44 -0400

    new installation

--- setup.php
+++ setup.php
@@ -1,5 +1,40 @@
 <?php
 
+/*
+ *
+  This file is part of the Modifications plugin.
+
+ Order plugin is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ Stevenes Donato; either version 2 of the License, or
+ (at your option) any later version.
+
+ Order plugin is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; along with itilcategorygroups. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ @package   modifications
+ @author    Stevenes Donato
+ @copyright Copyright (c) 2020 Stevenes Donato
+ @license   GPLv3
+            http://www.gnu.org/licenses/gpl.txt
+ @link      https://github.com/stdonato/glpi-modifications
+ @link      http://www.glpi-project.org/
+ @since     2018
+ --------------------------------------------------------------------------
+ */
+
+/**
+ * @name plugin_mod_install
+ * @access public
+ * @return boolean
+ */
+
+
 function plugin_init_mod() {
   
    global $PLUGIN_HOOKS, $LANG ;
@@ -14,10 +49,11 @@ function plugin_init_mod() {
 	   ]);
 	             
 	   $PLUGIN_HOOKS['config_page']['mod'] = 'config.php';
-	   //$PLUGIN_HOOKS['add_javascript']['mod'] = "scripts/mod.js";
 	   $PLUGIN_HOOKS['add_javascript']['mod'][] = "scripts/stats.js";
 	   $PLUGIN_HOOKS['add_javascript']['mod'][] = "scripts/ind.js";
- 	}                    
+ 	}  
+ 	
+ 	include('install.php');                     
 }
 
 
